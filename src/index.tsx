51f8d39d8f4ceee89ee14a9b20fd71ddbb9f2361
kirsenkodanit <a.kirs@ukr.net>
2020-06-07 16:26:21 +0300

    create store

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 import './css/reset.css';
 import './css/index.css';
 import { App } from './App';
+import store from './store/index';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
